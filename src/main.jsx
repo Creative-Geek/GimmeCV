58bd9947b1467113be6725208559a6893f3bd326
Creative-Geek <ahmedtaha1234@gmail.com>
2026-05-18 23:42:49 +0300

    Integrate Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { ToastProvider } from "./components/Toast.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics />
     <ToastProvider>
       <App />
     </ToastProvider>
